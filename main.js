fe5df7687f4d2d2d9308a56e628ea5b0243f6163
jmgomezco <jmgomezco@gmail.com>
2025-09-07 20:00:32 +0200

    Tu mensaje de commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function sendEmail() {
+    window.location.href = "mailto:juan@nhug.ai";
+}
+
+function callCie10Api() {
+    // Redirigir directamente a la API sin tokens
+    window.location.href = 'https://cie10.nhug.ai/';
+}
