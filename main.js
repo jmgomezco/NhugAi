891968c9f7b1309236afbba4dd1b2075f5161d7a
jmgomezco <jmgomezco@gmail.com>
2025-09-23 17:25:30 +0200

    Update window dimensions for email and API calls

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function sendEmail() {
-    window.open("mailto:nhugai@nhug.ai", '', 'width=800,height=600');
+    window.open("mailto:nhugai@nhug.ai", '', 'width=1000,height=1000');
     window.close();
 }
 
 function callCie10Api() {
-    window.open("https://cie10.nhug.ai", '', 'width=1280,height=720');
+    window.open("https://cie10.nhug.ai", '', 'width=1920,height=1080');
     window.close();
 }
 
 function goToNhugIn() {
-    window.open("https://in.nhug.ai", '', 'width=1280,height=720');
+    window.open("https://in.nhug.ai", '', 'width=1920,height=1080');
     window.close();
 }
