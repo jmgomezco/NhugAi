d933d40dc458edf68b791d3c9b87be0c16a61a46
jmgomezco <jmgomezco@gmail.com>
2025-09-23 08:07:09 +0200

    Close window after opening links in main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 function sendEmail() {
     window.open("mailto:nhugai@nhug.ai", "_blank");
+    window.close();
 }
 
 function callCie10Api() {
     window.open("https://cie10.nhug.ai", "_blank");
+    window.close();
 }
 
 function goToNhugIn() {
     window.open("https://in.nhug.ai", "_blank");
+    window.close();
 }
