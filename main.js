985e272b896155266ed6ae27433c75132994d66b
jmgomezco <jmgomezco@gmail.com>
2025-09-24 10:06:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 function sendEmail() {
     window.open("mailto:nhugai@nhug.ai", '', 'width=1000,height=1000');
-    window.close();
-}
+   }
 
 function callCie10Api() {
     window.open("https://cie10.nhug.ai", '', 'width=1920,height=1080');
-    window.close();
-}
+    }
 
 function goToNhugIn() {
     window.open("https://in.nhug.ai", '', 'width=1920,height=1080');
-    window.close();
-}
+   }
